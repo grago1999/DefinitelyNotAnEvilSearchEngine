84905f0c717e24178fd30f54a15af0aa5060a5d2
grago1999 <grago1999@gmail.com>
2016-08-13 11:52:04 -0400

    something

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 
 var SEARCH_TEXT;
-var suggest = function(current_text){
-  console.log(current_text);
-  SEARCH_TEXT = current_text;
 
+var suggest = function(current_text){
+  SEARCH_TEXT = current_text;
 };
 var search = function(){
-  console.log(SEARCH_TEXT);
+  $(location).attr('href', 'search.html');
 };
