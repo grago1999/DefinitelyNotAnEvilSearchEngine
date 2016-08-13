16e891e381114b437454532add3f107393dde5c6
grago1999 <grago1999@gmail.com>
2016-08-13 10:50:15 -0400

    Get bing

--- bingSearch.js
+++ bingSearch.js
@@ -1,11 +1,24 @@
-function getSiteData() {
+function getSiteData(searchTerm, index) {
   $.ajax({
-      type: 'GET',
-      url: 'https://api.github.com/users/mralexgray/repos',
-      data: { get_param: 'value' },
+      method: 'post',
+      url: 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+searchTerm+'%27&$skip=1000',
       dataType: 'json',
+      headers: {
+        'Authorization': 'Basic ' + btoa(':7oc2w5VV/beWzXSDzhB3dpk6CDHSH7lD/5z82UH84Us:')
+      },
       success: function (data) {
-          console.log(data);
+          if (data.d.results.length > 0) {
+            console.log(data);
+          } else {
+            if (index == 1000) {
+              getSiteData(index-100);
+            } else {
+              getSiteData(index-1000);
+            }
+          }
+      },
+      failure: function(err) {
+          console.error(err);
       }
   });
 }
