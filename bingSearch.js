cdb55e368162a01bf5d8ffa5340373d8838cb5bf
grago1999 <grago1999@gmail.com>
2016-08-13 11:18:56 -0400

    "Better" search

--- bingSearch.js
+++ bingSearch.js
@@ -1,19 +1,26 @@
 function getSiteData(searchTerm, index) {
   $.ajax({
       method: 'post',
-      url: 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+searchTerm+'%27&$skip=1000',
+      url: 'https://api.datamarket.azure.com/Bing/Search/v1/Web?Query=%27'+searchTerm+'%27&$skip='+index,
       dataType: 'json',
       headers: {
         'Authorization': 'Basic ' + btoa(':7oc2w5VV/beWzXSDzhB3dpk6CDHSH7lD/5z82UH84Us:')
       },
-      success: function (data) {
-          if (data.d.results.length > 0) {
-            console.log(data);
+      success: function(data) {
+          var results = data.d.results;
+          if (results.length > 8) {
+            var resultsHTML = '';
+            for (var i = 0; i < results.length; i++) {
+                var result = results[i];
+                var div = '<div>'+result.Title+' '+result.Url+'</div>';
+                resultsHTML += div;
+            }
+            $('#results').html(resultsHTML);
           } else {
             if (index == 1000) {
-              getSiteData(index-100);
+              getSiteData(searchTerm, index-100);
             } else {
-              getSiteData(index-1000);
+              getSiteData(searchTerm, index-1000);
             }
           }
       },
